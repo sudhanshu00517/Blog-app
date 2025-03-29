7fe6897b90d2d93753755249779b8e5dd27c3553
sudhanshu00517 <sudhanshu00517@gmail.com>
2025-03-30 00:43:13 +0530

    blog app setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let posts = [];  // Store posts temporarily
+
+app.get("/", (req, res) => {
+  res.render("index", { posts });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/new", (req, res) => {
+  const { title, content } = req.body;
+  posts.push({ title, content });  // Add post to array
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
